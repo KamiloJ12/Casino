99f2246722351e7597078403e67d9449ff0be62e
Brayan <tronks_465@hotmail.com>
2022-05-31 18:57:52 -0500

    ci

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { CasinoComponent } from './components/casino/casino.component';
 import { CasinoEnVivoComponent } from './components/casino-en-vivo/casino-en-vivo.component';
+import { TorneoComponent } from './components/torneo/torneo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     CasinoComponent,
-    CasinoEnVivoComponent
+    CasinoEnVivoComponent,
+    TorneoComponent
   ],
   imports: [
     BrowserModule,
