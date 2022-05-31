c99f293f3d32df1ca8df806aa82daac8460c2c9b
KamiloJ12 <camilo12052000@hotmail.com>
2022-05-31 17:58:58 -0500

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CasinoEnVivoComponent } from './components/casino-en-vivo/casino-en-vivo.component';
 import { CasinoComponent } from './components/casino/casino.component';
 
 const routes: Routes = [
-  {path:'', component: CasinoComponent}
+  {path:'', component: CasinoComponent},
+  {path:'casinoEnVivo', component: CasinoEnVivoComponent}
 ];
 
 @NgModule({
