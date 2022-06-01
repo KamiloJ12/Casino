a703ddd8260afdd81edbe6968fe342cd593ae5be
KamiloJ12 <camilo12052000@hotmail.com>
2022-05-31 20:53:53 -0500

    first commit

--- src/app/components/torneo/videojuegos.ts
+++ src/app/components/torneo/videojuegos.ts
@@ -1,9 +1,9 @@
 export const videojuegos = [
     {
-        imagen: '../../../assets/image/tourn3.jpg',   
+        imagen: '../../../assets/image/tourn3.jpg',  
     },
     {
-        imagen: '../../../assets/image/tourn1.jpg',    
+        imagen: '../../../assets/image/tourn1.jpg',  
     },
     {
         imagen: '../../../assets/image/tourn2.jpg',
