a62902676bd6adf00dfb4ac839c19624548021b9
KamiloJ12 <camilo12052000@hotmail.com>
2022-05-31 20:19:35 -0500

    first commit

--- src/app/components/casino/videojuegos.ts
+++ src/app/components/casino/videojuegos.ts
@@ -1,48 +1,62 @@
 export const videojuegos = [
     {
-        imagen: '../../../assets/image/img1.jpg',   
+        imagen: '../../../assets/image/img1.jpg',
+        nombre: 'Gemini Joker'   
     },
     {
-        imagen: '../../../assets/image/img2.jpg',    
+        imagen: '../../../assets/image/img2.jpg', 
+        nombre: 'Alkemors Elements'   
     },
     {
         imagen: '../../../assets/image/img3.jpg',
+        nombre: 'Kensei Blades'
     },
     {
         imagen: '../../../assets/image/img4.jpg',
+        nombre: 'Lost Mystery Chests'
+    },
+    {
+        imagen: '../../../assets/image/img5.jpg',
+        nombre: 'Primal Wilderness'
     },
     {   
-        imagen: '../../../assets/image/img5.jpg',   
+        imagen: '../../../assets/image/img6.jpg',
+        nombre: 'Gold Tiger Ascent'   
     },
     {
-        imagen: '../../../assets/image/img6.jpg',   
+        imagen: '../../../assets/image/img7.jpg',
+        nombre: 'Take the Kingdom'   
     },
     {
-        imagen: '../../../assets/image/img7.jpg',   
+        imagen: '../../../assets/image/img8.jpg',
+        nombre: 'Thai Blossoms'   
     },
     {
-        imagen: '../../../assets/image/img8.jpg',   
+        imagen: '../../../assets/image/img9.jpg',
+        nombre: 'Return to Paris'   
     },
     {
-        imagen: '../../../assets/image/img9.jpg',   
+        imagen: '../../../assets/image/img10.jpg',
+        nombre: 'Triple Juicy Drops'   
     },
     {
-        imagen: '../../../assets/image/img10.jpg',   
+        imagen: '../../../assets/image/img11.jpeg',
+        nombre: '7 Fortune Frenzy'   
     },
     {   
-        imagen: '../../../assets/image/img11.jpeg',   
+        imagen: '../../../assets/image/img12.jpeg',
+        nombre: 'Tower of Fortuna'   
     },
     {
-        imagen: '../../../assets/image/img12.jpeg',   
+        imagen: '../../../assets/image/img13.jpeg',
+        nombre: '88 Frenzy Frenzy'   
     },
     {
-        imagen: '../../../assets/image/img13.jpeg',   
+        imagen: '../../../assets/image/img14.jpeg',
+        nombre: 'Wild Drops'   
     },
     {
-        imagen: '../../../assets/image/img14.jpeg',   
-    },
-    {
-        imagen: '../../../assets/image/img15.jpeg',   
+        imagen: '../../../assets/image/img15.jpeg',
+        nombre: 'Stay Frosty!'   
     }
-
 ]
