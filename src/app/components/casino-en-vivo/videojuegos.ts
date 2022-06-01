27cbe40e20d36e2ac163a68107f3d851540ad4e2
KamiloJ12 <camilo12052000@hotmail.com>
2022-05-31 20:27:15 -0500

    first commit

--- src/app/components/casino-en-vivo/videojuegos.ts
+++ src/app/components/casino-en-vivo/videojuegos.ts
@@ -1,48 +1,63 @@
 export const videojuegos = [
     {
-        imagen: '../../../assets/image/vivo1.webp',   
+        imagen: '../../../assets/image/vivo1.webp',
+        nombre: 'XL Roulette'   
     },
     {
-        imagen: '../../../assets/image/vivo2.webp',    
+        imagen: '../../../assets/image/vivo2.webp',
+        nombre: 'Casino Hold\'em'    
     },
     {
         imagen: '../../../assets/image/vivo3.webp',
+        nombre: 'Lucky 6'
     },
     {
         imagen: '../../../assets/image/vivo4.webp',
+        nombre: '1Bet'
     },
     {   
-        imagen: '../../../assets/image/vivo5.webp',   
+        imagen: '../../../assets/image/vivo5.webp',
+        nombre: 'Keno'   
     },
     {
-        imagen: '../../../assets/image/vivo6.webp',   
+        imagen: '../../../assets/image/vivo6.webp',
+        nombre: 'JokerBet'   
     },
     {
-        imagen: '../../../assets/image/vivo7.webp',   
+        imagen: '../../../assets/image/vivo7.webp',
+        nombre: '5Bet'   
     },
     {
-        imagen: '../../../assets/image/vivo8.webp',   
+        imagen: '../../../assets/image/vivo8.webp',
+        nombre: 'War of elements'   
     },
     {
-        imagen: '../../../assets/image/vivo9.webp',   
+        imagen: '../../../assets/image/vivo9.webp',
+        nombre: '21Bet'   
     },
     {
-        imagen: '../../../assets/image/vivo10.webp',   
+        imagen: '../../../assets/image/vivo10.webp',
+        nombre: 'PokerBet'   
     },
     {
-        imagen: '../../../assets/image/vivo11.webp',   
+        imagen: '../../../assets/image/vivo11.webp',
+        nombre: 'WheelBet'   
     },
     {
-        imagen: '../../../assets/image/vivo12.webp',   
+        imagen: '../../../assets/image/vivo12.webp',
+        nombre: 'Lobby'   
     },
     {
-        imagen: '../../../assets/image/vivo13.webp',   
+        imagen: '../../../assets/image/vivo13.webp',
+        nombre: '7Bet'   
     },
     {
-        imagen: '../../../assets/image/vivo14.webp',   
+        imagen: '../../../assets/image/vivo14.webp',
+        nombre: 'Hypergammon'   
     },
     {
-        imagen: '../../../assets/image/vivo15.webp',   
+        imagen: '../../../assets/image/vivo15.webp',
+        nombre: 'Classic Wheel'   
     }
 
 ]
