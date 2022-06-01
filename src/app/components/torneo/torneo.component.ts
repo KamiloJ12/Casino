4582e00e1806504ac1a77c31d5eec74da6224cd8
Brayan <tronks_465@hotmail.com>
2022-05-31 19:57:27 -0500

    ci

--- src/app/components/torneo/torneo.component.ts
+++ src/app/components/torneo/torneo.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-
+import { videojuegos } from './videojuegos';
 @Component({
   selector: 'app-torneo',
   templateUrl: './torneo.component.html',
   styleUrls: ['./torneo.component.css']
 })
 export class TorneoComponent implements OnInit {
+
+  public videojuegos: Array<any> = videojuegos; 
 
   constructor() { }
 
---
+++ src/app/components/torneo/videojuegos.ts
@@ -0,0 +1,13 @@
+export const videojuegos = [
+    {
+        imagen: '../../../assets/image/vivo1.webp',   
+    },
+    {
+        imagen: '../../../assets/image/vivo2.webp',    
+    },
+    {
+        imagen: '../../../assets/image/vivo3.webp',
+    },
+  
+
+]
