cd49f7e9aab13361d9a8c93931e5ed96971cdd32
KamiloJ12 <camilo12052000@hotmail.com>
2022-05-31 19:04:01 -0500

    first commit

--- src/app/components/casino-en-vivo/casino-en-vivo.component.ts
+++ src/app/components/casino-en-vivo/casino-en-vivo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { videojuegos } from './videojuegos';
 
 @Component({
   selector: 'app-casino-en-vivo',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./casino-en-vivo.component.css']
 })
 export class CasinoEnVivoComponent implements OnInit {
+
+  public videojuegos: Array<any> = videojuegos; 
 
   constructor() { }
 
---
+++ src/app/components/casino-en-vivo/videojuegos.ts
@@ -0,0 +1,48 @@
+export const videojuegos = [
+    {
+        imagen: '../../../assets/image/vivo1.webp',   
+    },
+    {
+        imagen: '../../../assets/image/vivo2.webp',    
+    },
+    {
+        imagen: '../../../assets/image/vivo3.webp',
+    },
+    {
+        imagen: '../../../assets/image/vivo4.webp',
+    },
+    {   
+        imagen: '../../../assets/image/vivo5.webp',   
+    },
+    {
+        imagen: '../../../assets/image/vivo6.webp',   
+    },
+    {
+        imagen: '../../../assets/image/vivo7.webp',   
+    },
+    {
+        imagen: '../../../assets/image/vivo8.webp',   
+    },
+    {
+        imagen: '../../../assets/image/vivo9.webp',   
+    },
+    {
+        imagen: '../../../assets/image/vivo10.webp',   
+    },
+    {
+        imagen: '../../../assets/image/vivo11.webp',   
+    },
+    {
+        imagen: '../../../assets/image/vivo12.webp',   
+    },
+    {
+        imagen: '../../../assets/image/vivo13.webp',   
+    },
+    {
+        imagen: '../../../assets/image/vivo14.webp',   
+    },
+    {
+        imagen: '../../../assets/image/vivo15.webp',   
+    }
+
+]
